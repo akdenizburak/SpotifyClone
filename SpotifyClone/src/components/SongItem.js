d1908398d4bd04feb3dd66df27c42a67a1bd4002
akdenizburak <akdenizburakk7@gmail.com>
2022-09-15 04:57:01 +0300

    Edited SonItem Component

--- SpotifyClone/src/components/SongItem.js
+++ SpotifyClone/src/components/SongItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { View, StyleSheet, Image, Text } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
-const SongItem = ({song,singer,avatarURL}) => {
+const SongItem = ({ song, singer, avatarURL }) => {
     return (
         <View style={styles.contentItem} >
             <Image style={styles.contentItemAvatar} source={{ uri: avatarURL }} />
             <View style={styles.contentItemInfo}>
                 <Text style={styles.contentItemSong} >{song}</Text>
                 <Text style={styles.contentItemSinger} >{singer}</Text>
+            </View>
+            <View style={styles.iconLike}>
+                <FontAwesome name="heart-o" size={25} color="green" />
             </View>
         </View>
     );
@@ -20,30 +24,34 @@ const styles = StyleSheet.create({
         height: 80,
         borderRadius: 7,
         flexDirection: "row",
-        alignItems:"center",
-        paddingHorizontal:10,
+        alignItems: "center",
+        justifyContent:"space-between",
+        paddingHorizontal: 10,
         backgroundColor: "#222",
     },
     contentItemAvatar: {
         backgroundColor: "white",
-        width:60,
-        height:60,
-        borderRadius:5
+        width: 60,
+        height: 60,
+        borderRadius: 5
     },
-    contentItemInfo:{
-        flexDirection:"column",
-        paddingLeft:20
+    contentItemInfo: {
+        flexDirection: "column",
+        marginLeft:-100
     },
     contentItemSong: {
-        fontSize:25,
-        color:"lightgray",
-        fontWeight:"600"
+        fontSize: 25,
+        color: "lightgray",
+        fontWeight: "600"
     },
     contentItemSinger: {
-        fontSize:17,
-        color:"lightgray",
-        fontWeight:"600"
+        fontSize: 17,
+        color: "lightgray",
+        fontWeight: "600"
     },
+    iconLike: {
+        marginRight:10
+    }
 })
 
 export default SongItem;
