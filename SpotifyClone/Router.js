74e018225ec06b6128f9b9e2d209848588e6276e
akdenizburak <akdenizburakk7@gmail.com>
2022-09-17 12:58:12 +0300

    Edited Router

--- SpotifyClone/Router.js
+++ SpotifyClone/Router.js
@@ -1,20 +1,9 @@
-// In App.js in a new project
-
 import React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import SignInPage from './src/pages/SignInPage';
 import SignUpPage from './src/pages/SignUpPage';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createNativeStackNavigator();
 
