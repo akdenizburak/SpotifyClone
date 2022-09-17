2c978a260c0b807329c4040bc72a1a362243e775
akdenizburak <akdenizburakk7@gmail.com>
2022-09-17 04:00:05 +0300

    Added Firebase

---
+++ SpotifyClone/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCMfajfPUP_g1kKdD2tKyHSIUQrSe8WqII",
+  authDomain: "spotifyclone-5a14c.firebaseapp.com",
+  projectId: "spotifyclone-5a14c",
+  storageBucket: "spotifyclone-5a14c.appspot.com",
+  messagingSenderId: "502901554924",
+  appId: "1:502901554924:web:bcf69382fca8545d3f6fc9"
+};
